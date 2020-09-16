ee7340409cbd9a4df384cad957c2979dea6d13e2
Wanvisa-Nantawong <nantawong199853@gmail.com>
2020-09-16 13:40:12 +0700

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,15 @@
+let PI = "4"
+name = "John"
+let test = "Hello"
+name = `${test}S`
+let arr = ['apple','banana','kiwi']
+console.log("arr :",arr[1])
+let person = {
+    name: 'JOKE',
+    age: '15',
+    gender:{ 
+        'male'
+    },
+    arr
+}
+console.log(person.gender)
