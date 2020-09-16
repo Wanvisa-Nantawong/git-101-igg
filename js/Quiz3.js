ce0045fc8e153c5a5f9e9c4e3874597cc8a790a2
Wanvisa-Nantawong <nantawong199853@gmail.com>
2020-09-16 15:22:23 +0700

    Create Quiz3.js

---
+++ js/Quiz3.js
@@ -0,0 +1,19 @@
+scoce = 55
+
+if ( 80 <scoce  && scoce<=100 ){
+    grade = "A";
+}
+else if (70 < scoce &&scoce <=80 ){
+    grade = "B";
+}
+else if (60 < scoce &&scoce<= 70){
+    grade = "C";
+}
+else if (50 < scoce &&scoce <=60){
+    grade = "D"
+}
+else if (0 < scoce &&scoce<=50){
+    grade ="F"
+}
+
+console.log(grade)
