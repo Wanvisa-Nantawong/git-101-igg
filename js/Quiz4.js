39a9da53d6d2f6ec8a5863eb50b7806b882f034d
Wanvisa-Nantawong <nantawong199853@gmail.com>
2020-09-16 15:40:56 +0700

    Create Quiz4.js

---
+++ js/Quiz4.js
@@ -0,0 +1,11 @@
+var str = "Hello my name is Joke"
+var n = str.length
+console.log(n)
+
+var str = "Hello my name is Joke nice to meet you!"
+var v = str.search("Joke")
+console.log(v)
+
+var str = "Well open your mind and see like me"
+var x = str.slice(5,9)
+console.log(x)
