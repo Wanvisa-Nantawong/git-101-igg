abcf616d621a7ed76ed4060753bfa539c9b26b70
Wanvisa-Nantawong <nantawong199853@gmail.com>
2020-09-16 14:05:55 +0700

    Create function.js
    Arrow fuction

---
+++ js/function.js
@@ -0,0 +1,21 @@
+//  function add(a, b){
+//     this.user = 'JOKE'
+//      return a + b
+//  }
+// console.log(this.user)
+
+// function mul(a, b){
+//     return a * b
+// }
+
+// function searchCustomer(customerName){
+//     find some customer
+// }
+const add =(a, b) => a + b
+    
+
+
+
+
+console.log(add(2,3))
+console.log(mul(2,3))
