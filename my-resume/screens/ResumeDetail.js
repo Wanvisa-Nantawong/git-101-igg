4ba6034be3a970fb680f28ac6b858ccfed81ccb7
Wanvisa-Nantawong <nantawong199853@gmail.com>
2020-09-18 15:20:07 +0700

    Update ResumeDetail.js

--- my-resume/screens/ResumeDetail.js
+++ my-resume/screens/ResumeDetail.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
+import {StyleSheet, View, Text } from 'react-native'
+import ResumeForm from './ResumeForm'
 
 export default (props) => {
-    const [rename, setResume] = useState({
+    
+    const [resume, setResume] = useState({
         name:'',
         nickname:'',
         age:'',
@@ -21,5 +24,19 @@ export default (props) => {
     }, []
     )
 
-    return null
+    return (
+        <View style={StyleSheet.container}>
+            <View style={styles.taxtLine}>
+                <Text>Full name : {resume.name}</Text>
+            </View>
+            <View style={styles.taxtLine}>
+                <Text>Nickname : {resume.nickname}</Text>
+            </View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {padding: 30 ,backgroundColor:'white', minHeight:'100%'},
+    taxtLine: {marginBottom: 20}
+})
