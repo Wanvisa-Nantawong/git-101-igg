ac45fb6086b7a81a2d91a9f3dc594d8be15967a5
Wanvisa-Nantawong <nantawong199853@gmail.com>
2020-09-18 12:05:12 +0700

    Update ResumeForm.js

--- my-resume/screens/ResumeForm.js
+++ my-resume/screens/ResumeForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ValidationComponent from 'react-native-form-validator'
-import {View, StyleSheet, Text, TextInput ,Button } from 'react-native'
+import {View, StyleSheet, Text, TextInput ,Button, Alert} from 'react-native'
+import axios from 'axios'
 
 export default class ResumeForm extends ValidationComponent{
     state = {
@@ -12,12 +13,39 @@ export default class ResumeForm extends ValidationComponent{
 
    
     _onSubmit = () => {
-        this.validate({
+        const isValid = this.validate({
             name:{required: true},
             nickname:{required: true},
             age:{required: true, numbers: true},
             skill:{required: true}
-        })
+        });
+        if (isValid) {
+            const formData = new FormData();
+            formData.append('name', this.state.name)
+            formData.append('nickname', this.state.nickname)
+            formData.append('age', this.state.age)
+            formData.append('skill', this.state.skill)
+
+            const config ={
+                headers: {'content-type' : 'multipart/form-data'}
+            }
+
+
+            axios.post('https://movie-api.igeargeek.com/users/register', formData,config)
+            .then((response) => {
+                Alert.alert(
+                    'Create success', 
+                    'Click Ok go to resume detail page'),
+                    [{
+                        test: 'OK',
+                        onPress: () => {
+                            this.props.navigation.push('ResumeDetail',{id: response.data.id})
+                        }
+                    }]
+            }).catch((error) => {
+                console.log('api error', error)
+            })
+        }
     }
 
     render() {
