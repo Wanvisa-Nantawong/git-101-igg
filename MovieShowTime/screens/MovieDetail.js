3c4ddc16e0df6fa769d2069a8b7614ca93b14e62
Wanvisa-Nantawong <nantawong199853@gmail.com>
2020-09-19 15:23:32 +0700

    Update MovieDetail.js

--- MovieShowTime/screens/MovieDetail.js
+++ MovieShowTime/screens/MovieDetail.js
@@ -1,9 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-export default function MovieDetail() {
+import React, { useEffect, useState }from 'react'
+import { View, Text, Image, StyleSheet, ScrollView} from 'react-native'
+import { WebView } from 'react-native-webview';
+import axios from 'axios'
+
+
+export default function MovieDetail({ navigation, route }) {
+    const [movies, setMovies] = useState({
+        genre: []
+    })
+    
+
+    useEffect(() => {
+        const itemId = route.params.id
+        axios.get(`https://movie-api.igeargeek.com/movies/${itemId}`)
+            .then(res => {
+            const movies = res.data;
+            setMovies(movies)
+        })
+      }, [])
+
+
     return (
-        <View>
-            <Text>Movie Detail</Text>
+        <ScrollView 
+        showsVerticalScrollIndicator={false}
+            style={{flex: 1}}
+            contentContainerStyle={{flexGrow: 1}}>
+        <View style={{flex: 1}}>
+            <WebView
+                allowsFullscreenVideo
+                source={{uri: movies.youtubeUrl}}
+                style={{height:200, flex: 1}}
+            />
+            <View style={{flex: 2, padding:15, backgroundColor: '#3399ff'}}>
+                <View style={{flexDirection:'row'}}>
+                    <Image 
+                    source={{uri: movies.posterUrl}} 
+                    style={{ flex: 1, height: 180 }}
+                    resizeMode={'stretch'}
+                    />
+                    <View style={{flex: 2, marginLeft: 15}}>
+                        <Text style={{ color: 'white', fontSize:17}}>{movies.name}</Text>
+                        <Text style={{color: 'white'}}>ประเภท : {movies.genre.join(',')}</Text>
+                    </View>
+            </View>
+            </View>
         </View>
+        </ScrollView>
     )
 }
